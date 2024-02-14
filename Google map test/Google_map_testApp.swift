94585849dc3318a142c662ec89fd2cfd23b8edaf
A3426L <mikan7738@gmail.com>
2024-02-15 00:57:26 +0900

    Initial Commit

---
+++ Google map test/Google_map_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Google_map_testApp.swift
+//  Google map test
+//
+//  Created by aru on 2024/02/15.
+//
+
+import SwiftUI
+
+@main
+struct Google_map_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
